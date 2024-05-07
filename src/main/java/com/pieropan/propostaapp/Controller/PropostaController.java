bcf02caa79dbbb720e12e00244bb045ea69bad87
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-05-06 21:43:39 -0300

    Criando Repository e Service

--- src/main/java/com/pieropan/propostaapp/Controller/PropostaController.java
+++ src/main/java/com/pieropan/propostaapp/Controller/PropostaController.java
@@ -1,6 +1,9 @@
 package com.pieropan.propostaapp.Controller;
 
 import com.pieropan.propostaapp.dto.PropostaRequestDto;
+import com.pieropan.propostaapp.dto.PropostaResponseDto;
+import com.pieropan.propostaapp.service.PropostaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    @Autowired
+    PropostaService propostaService;
+
     @PostMapping
-    public ResponseEntity criar(@RequestBody PropostaRequestDto requestDto) {
-        return null;
+    public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
+        PropostaResponseDto response = propostaService.criar(requestDto);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -0,0 +1,17 @@
+package com.pieropan.propostaapp.service;
+
+import com.pieropan.propostaapp.dto.PropostaRequestDto;
+import com.pieropan.propostaapp.dto.PropostaResponseDto;
+import com.pieropan.propostaapp.repository.PropostaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaService {
+
+    private PropostaRepository propostaRepository;
+
+    public PropostaResponseDto criar(PropostaRequestDto propostaRequestDto){
+        propostaRepository.save();
+        return null;
+    }
+}
